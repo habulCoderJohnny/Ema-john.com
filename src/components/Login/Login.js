69290d54f177508b97367408608f7d2803461796
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-13 01:50:03 +0600

    plain css style Login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='form-container'>
+            <div>
+                <h1 className='form-title'>Login</h1>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>
+                    New to Ema-john? <Link className='form-link' to="/signUp">Create an account</Link>
+                </p>
+            </div>
         </div>
     );
 };
