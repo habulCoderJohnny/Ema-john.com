dd90bb4720051c8c20d472460d403248cb91653a
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-13 23:50:15 +0600

    roll ternary operator in login-logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 
 const Header = () => {
+    //all link apply er por declore
+    const [user] = useAuthState(auth);
     return (
         <nav className='Header'>
             <img src={logo}alt="amazon's logo"/>
@@ -13,7 +17,11 @@ const Header = () => {
                 <Link to="/orders">Order</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
+                { user?
+                <button className='signout-btn'>Sign Out</button>
+                :
                 <Link to="/login">Login</Link>
+                }
                 {/* <Link to="/signup">SignUp</Link> */}
             </div>
         </nav>
